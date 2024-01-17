9581b2bc8ebeaade115511408630cb83e739f9b0
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-01-17 17:04:39 +0900

    feat: 헤더 뱃지

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -17,3 +17,26 @@ searchInputEl.addEventListener('blur', () => {
   searchInputEl.setAttribute('placeholder', '');
   //   html 속성을 지정한다.
 });
+
+const badgeEl = document.querySelector('header .badges');
+window.addEventListener(
+  'scroll',
+  _.throttle(() => {
+    // console.log(window.scrollY);
+    // badgeEl.style.display = window.scrollY > 500 ? 'none' : 'block';
+
+    if (window.scrollY > 500) {
+      // gsap.to(요소,지속시간,옵션)
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+        // 숫자는 그대로 작성하는데 문자열은 '' 에 감싸기
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300)
+);

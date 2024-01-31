7d834cef513c1936a529dcd159750071699a3b6a
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-01-31 15:22:43 +0900

    chore: 기타

--- tictactoe/src/Controller.js
+++ tictactoe/src/Controller.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Controller extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Next Player : {this.props.player}</h2>
-        <button>Go to game start</button>
-        <button>Go to move #1</button>
-      </div>
-    );
-  }
-}
+const Controller = ({ player }) => {
+  return (
+    <div>
+      <h2>Next Player : {player}</h2>
+      <button>Go to game start</button>
+      <button>Go to move #1</button>
+    </div>
+  );
+};
 
 export default Controller;
--- tictactoe/src/component/Board.js
+++ tictactoe/src/component/Board.js
@@ -1,50 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 import './Board.css';
-export default class Board extends Component {
-  constructor(props) {
-    super(props);
+const Board = ({ player, setPlayer }) => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+    newSquares[i] = player;
+    setSquares(newSquares);
+    setPlayer();
+  };
+  const renderSqure = (i) => {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  };
 
-    this.state = {
-      squares: Array(9).fill(null),
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = this.props.player;
-    this.setState({ squares: squares });
-    this.props.setPlayer();
-  }
-  renderSqure(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <h1 className="status">Tic Tac Toe</h1>
-        <div className="board-row">
-          {this.renderSqure(0)}
-          {this.renderSqure(1)}
-          {this.renderSqure(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSqure(3)}
-          {this.renderSqure(4)}
-          {this.renderSqure(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSqure(6)}
-          {this.renderSqure(7)}
-          {this.renderSqure(8)}
-        </div>
+  return (
+    <div>
+      <h1 className="status">Tic Tac Toe</h1>
+      <div className="board-row">
+        {renderSqure(0)}
+        {renderSqure(1)}
+        {renderSqure(2)}
       </div>
-    );
-  }
-}
+      <div className="board-row">
+        {renderSqure(3)}
+        {renderSqure(4)}
+        {renderSqure(5)}
+      </div>
+      <div className="board-row">
+        {renderSqure(6)}
+        {renderSqure(7)}
+        {renderSqure(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Board;

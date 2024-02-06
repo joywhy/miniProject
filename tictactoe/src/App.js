9cdd7b1b9d009806839830c9d386392d90723d81
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-02-06 19:10:43 +0900

    feat: history 기능

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,9 +1,58 @@
 import Board from './component/Board';
 import './App.css';
+import Controller from './component/Controller.js';
+import { useState } from 'react';
 function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xisNext, setXisNext] = useState(true);
+  const currentSquares = history[history.length - 1].squares;
+
+  const handleClick = (i) => {
+    if (winner || currentSquares[i]) return;
+
+    const newSquares = currentSquares.slice();
+    newSquares[i] = xisNext ? 'X' : 'O';
+    setHistory([...history, { squares: newSquares }]);
+    setXisNext(!xisNext);
+  };
+
+  const calculateWinner = (squares) => {
+    const winningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winningConditions.length; i++) {
+      const [a, b, c] = winningConditions[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  };
+  const winner = calculateWinner(currentSquares);
+  let status;
+
+  if (winner) {
+    status = `Winner : ${winner}`;
+  } else {
+    status = `Next Player : ${xisNext ? 'X' : 'O'}`;
+  }
+
   return (
     <div className="app">
-      <Board />
+      <Board onClick={(i) => handleClick(i)} squares={currentSquares} />
+      <Controller status={status} />
     </div>
   );
 }
--- tictactoe/src/component/Board.js
+++ tictactoe/src/component/Board.js
@@ -1,58 +1,11 @@
 import React, { useState } from 'react';
 import Square from './Square';
-import Controller from './Controller.js';
+
 import './Board.css';
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xisNext, setXisNext] = useState(true);
-
-  const handleClick = (i) => {
-    if (winner || squares[i]) return;
-
-    const newSquares = squares.slice();
-    newSquares[i] = xisNext ? 'X' : 'O';
-    setSquares(newSquares);
-    setXisNext(!xisNext);
-    //calculateWinner(squares); //그전 상태값이 출력됨.
-  };
+const Board = ({ onClick, squares }) => {
   const renderSqure = (i) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
   };
-
-  const calculateWinner = (squares) => {
-    // console.log(squares);
-    const winningConditions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < winningConditions.length; i++) {
-      const [a, b, c] = winningConditions[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        // console.log('winner!' + squares[a]);
-        return squares[a];
-      }
-    }
-    // console.log('not yet');
-    return null;
-  };
-  const winner = calculateWinner(squares);
-  let status;
-
-  if (winner) {
-    status = `Winner : ${winner}`;
-  } else {
-    status = `Next Player : ${xisNext ? 'X' : 'O'}`;
-  }
 
   return (
     <div>
@@ -72,7 +25,6 @@ const Board = () => {
         {renderSqure(7)}
         {renderSqure(8)}
       </div>
-      <Controller status={status} />
     </div>
   );
 };

2103b1724d3e0bc54f681e4eec49d276308366b8
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-02-06 16:54:07 +0900

    refactor: 상태를 텍스트로 묶기

--- tictactoe/src/Controller.js
+++ tictactoe/src/Controller.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Controller = ({ player }) => {
+  const status = `Next Player : ${player}`;
   return (
     <div>
-      <h2>Next Player : {player}</h2>
+      <h2>{status}</h2>
       <button>Go to game start</button>
       <button>Go to move #1</button>
     </div>

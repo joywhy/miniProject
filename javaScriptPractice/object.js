91645e03ccae05eec5c633480f1604c37e2b7e5c
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-02-18 16:43:36 +0900

    feat: 생성자 함수 메서드 중복 생성 중복 소유

--- javaScriptPractice/object.js
+++ javaScriptPractice/object.js
@@ -3,6 +3,17 @@
 // };
 // console.log(Object.values(obj));
 
+function Circle1(radius) {
+  this.radius = radius;
+  this.getArea = function () {
+    return Math.PI * this.radius ** 2;
+  };
+}
+const circle3 = new Circle1(1);
+const circle4 = new Circle1(2);
+
+console.log(circle3.getArea === circle4.getArea); //false
+
 function Circle(radius) {
   this.radius = radius;
 }
